743c9c844c5f1fabfab89d72cc2cb4c1e9a9e863
deimon005 <134963531+deimon005@users.noreply.github.com>
2023-11-01 20:46:11 -0300

    se crea cubiculos
    hola

---
+++ proyecto/cubiculos.js
@@ -0,0 +1,9 @@
+function showCubiculoInfo(element) {
+    var cubiculoInfo = element.querySelector('.cubiculo-info');
+    cubiculoInfo.style.display = 'block';
+}
+
+function hideCubiculoInfo(element) {
+    var cubiculoInfo = element.querySelector('.cubiculo-info');
+    cubiculoInfo.style.display = 'none';
+}
